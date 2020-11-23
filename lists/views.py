b0eb2bedf5e33904b8ace4c14d72edd3b3b4d2e1
SHLOK WALIA <virgoshlok@gmail.com>
2020-11-23 21:25:03 +0530

    added new URL and changed views

--- lists/views.py
+++ lists/views.py
@@ -2,15 +2,11 @@ from django.shortcuts import redirect, render
 from lists.models import Item
 
 def home_page(request):
-    new_item_text = ''
+    return render(request, 'home.html')
 
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world')
-
-    items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
     items = Item.objects.all()
